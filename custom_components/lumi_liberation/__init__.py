6341ee2868f8b577ce514c08e23e9806a5a2c117
Nguyên Minh <hknmmusic123@gmail.com>
2026-07-06 13:03:44 +0700

    fixed hacs.json

--- custom_components/lumi_liberation/__init__.py
+++ custom_components/lumi_liberation/__init__.py
@@ -19,21 +19,5 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         "port": port
     }
     
-    async def mqtt_message_received(msg):
-        payload = json.loads(msg.payload)
-        
-        # PROTCOL PARSING: You need to extract the specific device ID from the broadcast
-        # This part depends on how your hub formats the 'status' JSON
-        objects = payload.get("objects", []) 
-        for obj in objects:
-            dev_id = obj.get("hash")  # This is the "zigbee-..." ID
-            device_state = obj.get("state") # e.g., {"on": True}
-            
-            # This is the magic part:
-            # Send a signal that ONLY the entity with this specific dev_id will hear
-            dispatcher_send(hass, f"{DOMAIN}_update_{dev_id}", device_state)
-
-    await mqtt.async_subscribe(hass, "component/dnet-relay/in/status", mqtt_message_received)
-    # Now forward the setup to switch.py
     await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
     return True
