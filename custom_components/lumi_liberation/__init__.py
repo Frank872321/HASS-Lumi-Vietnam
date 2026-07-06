1f82da7866b3750641059f1a172dca3b6bde7ce4
Nguyên Minh <hknmmusic123@gmail.com>
2026-07-06 10:56:19 +0700

    fixed hacs.json

--- custom_components/lumi_liberation/__init__.py
+++ custom_components/lumi_liberation/__init__.py
@@ -1,6 +1,6 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from . import DOMAIN
+from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
--- custom_components/lumi_liberation/config_flow.py
+++ custom_components/lumi_liberation/config_flow.py
@@ -3,29 +3,10 @@ from homeassistant import config_entries
 from .const import DOMAIN
 @config_entries.config_flow_handler(DOMAIN)
 class LumiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Lumi Liberation."""
-    VERSION = 1
-
-    async def async_step_user(self, user_input=None):
-        """Handle the initial user step."""
-        errors = {}
-
-        if user_input is not None:
-            # Here you would typically validate the connection
-            # For now, we just save the data provided by the user
-            return self.async_create_entry(
-                title="Lumi Hub", 
-                data=user_input
-            )
-
-        # Define the form schema
-        data_schema = vol.Schema({
-            vol.Required("host", default="192.168.1.94"): str,
-            vol.Required("port", default=1883): int,
-        })
+    async def async_step_user(self, info):
+        if info is not None:
+            pass  # TODO: process info
 
         return self.async_show_form(
-            step_id="user", 
-            data_schema=data_schema, 
-            errors=errors
+            step_id="user", data_schema=vol.Schema({vol.Required("password"): str})
         )
