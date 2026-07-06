80c9bee0c4235efd647fbe58e09dbe944ee081c8
Nguyên Minh <hknmmusic123@gmail.com>
2026-07-06 11:50:01 +0700

    fixed hacs.json

--- custom_components/lumi_liberation/config_flow.py
+++ custom_components/lumi_liberation/config_flow.py
@@ -9,8 +9,8 @@ from .const import DOMAIN
 class LumiConfigFlow(ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, info):
         if info is not None:
-            pass  # TODO: process info
+            return self.async_create_entry(title="Lumi Hub", data=info)
 
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema({vol.Required("host"): str})
+            step_id="user", data_schema=vol.Schema({vol.Required("host"): str, vol.Required("port"): int})
         )
