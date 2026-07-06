fc9b73a5773397fca87d26b5b0ac4627e45cfd49
Nguyên Minh <hknmmusic123@gmail.com>
2026-07-06 13:25:21 +0700

    fixed hacs.json

--- custom_components/lumi_liberation/switch.py
+++ custom_components/lumi_liberation/switch.py
@@ -19,13 +19,13 @@ class LumiSwitch(SwitchEntity):
 
     async def async_turn_on(self, **kwargs):
         # Your reverse-engineered command payload
-        payload = """{
+        payload = {
             "cmd": "set","control_source": {"id": "ha-dev", "type": "app"},
-            "objects": [{"data": [self._devid], "execution": {"command": "OnOff", "params": {"on": true}}, "type":"devices"}],
+            "objects": [{"data": [self._devid], "execution": {"command": "OnOff", "params": {"on": True}}, "type":"devices"}],
             "reqid": "ha-dev",
             "source":"core",
             "timestamp": 0,
-        }"""
+        }
         await mqtt.async_publish(self.hass, "component/hc-zb/control", json.dumps(payload))
         self._attr_is_on = True
         self.async_write_ha_state()
@@ -34,7 +34,7 @@ class LumiSwitch(SwitchEntity):
         payload = {
             "cmd": "set",
             "control_source": {"id": "ha-dev", "type": "app"},
-            "objects": [{"data": [self._devid], "execution": {"command": "OnOff", "params": {"on": "false"}}, "type":"devices"}],
+            "objects": [{"data": [self._devid], "execution": {"command": "OnOff", "params": {"on": False}}, "type":"devices"}],
             "reqid": "ha-dev",
             "source":"core",
             "timestamp": 0,
